5be759b5f568242e6945802d98cdf53672d1188e
Kaushik Mondal <kaushikmondal.dev@gmail.com>
2026-03-25 18:40:17 +0530

    Basic Calculator Ui

---
+++ src/app/advanced/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Advanced | Calculator App",
+  description: "Advanced Calculator App",
+};
+
+const page = () => {
+  return <></>;
+};
+
+export default page;
---
+++ src/components/BasicCalculator.tsx
@@ -0,0 +1,40 @@
+import { Button } from "./shadcnui/button";
+import { Input } from "./shadcnui/input";
+
+const BasicCalculator = () => {
+  return (
+    <section className="grid grid-cols-4 gap-6">
+      <Input
+        type="number"
+        className="col-span-2"
+        placeholder="Number 1"
+      />
+      <Input
+        type="number"
+        className="col-span-2"
+        placeholder="Number 2"
+      />
+
+      <Input
+        type="number"
+        className="col-span-4"
+        placeholder="Total"
+        disabled
+      />
+
+      <Button type="button">Add</Button>
+      <Button type="button">Subtract</Button>
+      <Button type="button">Multiply</Button>
+      <Button type="button">Divide</Button>
+
+      <Button
+        type="button"
+        variant={"destructive"}
+        className="col-span-4">
+        Reset
+      </Button>
+    </section>
+  );
+};
+
+export default BasicCalculator;
