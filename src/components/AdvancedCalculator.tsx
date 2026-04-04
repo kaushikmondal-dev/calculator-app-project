aee0094e4cfc6b19ff7073eb2dfd016583c4b3b9
Kaushik Mondal <kaushikmondal.dev@gmail.com>
2026-04-04 13:29:22 +0530

    Advanced Calculator Logic

--- src/components/AdvancedCalculator.tsx
+++ src/components/AdvancedCalculator.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { DivideIcon, MinusIcon, PlusIcon, XIcon } from "lucide-react";
+import { useState } from "react";
 import { Button } from "./shadcnui/button";
 import { Input } from "./shadcnui/input";
 import {
@@ -10,15 +13,55 @@ import {
 } from "./shadcnui/select";
 
 const AdvancedCalculator = () => {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [operator, setoperator] = useState("");
+  const [total, setTotal] = useState("");
+
+  const handleCalculate = () => {
+    if (number1 !== "" && number2 !== "" && operator !== "") {
+      const num1 = parseInt(number1);
+      const num2 = parseInt(number2);
+
+      switch (operator) {
+        case "+":
+          setTotal((num1 + num2).toString());
+          break;
+
+        case "-":
+          setTotal((num1 - num2).toString());
+          break;
+
+        case "x":
+          setTotal((num1 * num2).toString());
+          break;
+
+        case "/":
+          setTotal((num1 / num2).toString());
+          break;
+      }
+      setNumber1("");
+      setNumber2("");
+      setoperator("");
+    }
+  };
+  const handleReset = () => {
+    setTotal("");
+  };
+
   return (
     <section className="grid grid-cols-3 gap-6">
       <Input
         type="number"
         className="col-span-1"
-        placeholder="1"
+        placeholder="Number 1"
+        onChange={(e) => setNumber1(e.target.value)}
+        value={number1}
       />
 
-      <Select>
+      <Select
+        onValueChange={(opValue) => setoperator(opValue ?? "")}
+        value={operator}>
         <SelectTrigger className="w-full">
           <SelectValue placeholder="Operator" />
         </SelectTrigger>
@@ -44,7 +87,9 @@ const AdvancedCalculator = () => {
       <Input
         type="number"
         className="col-span-1"
-        placeholder="2"
+        placeholder="Number 2"
+        onChange={(e) => setNumber2(e.target.value)}
+        value={number2}
       />
 
       <Input
@@ -52,17 +97,22 @@ const AdvancedCalculator = () => {
         className="col-span-2"
         placeholder="Total"
         disabled
+        value={total}
       />
       <Button
         type="button"
-        className="col-span-1">
+        className="col-span-1"
+        onClick={handleCalculate}
+        disabled={number1 === "" || number2 === "" || operator === ""}>
         Calculate
       </Button>
 
       <Button
         type="button"
         variant={"destructive"}
-        className={"col-span-3"}>
+        className={"col-span-3"}
+        onClick={handleReset}
+        disabled={(number1 === "" || number2 === "") && total === ""}>
         Reset
       </Button>
     </section>
